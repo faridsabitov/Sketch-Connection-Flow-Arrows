90155c23b19fb6e90d079beb82139301e4312c3c
Farid Sabitov <farid.sabitov.design@gmail.com>
2018-12-10 21:11:34 +0300

    Selection Logic

--- userflows/src/script.js
+++ userflows/src/script.js
@@ -3,38 +3,52 @@ const { toArray } = require('util')
 
 
 export default function() {
+  // Predefing
   const document = sketch.fromNative(context.document)
   const page = document.selectedPage
-  var selection = document.selectedLayers
+  // var selection = document.selectedLayers
+  var selection = context.selection
 
-  log(selection)
+  if(selection.count() == 2){
+    // When user selected two layers
+    
+    if(selection[1].class() == "MSSymbolInstance"){
+      sketch.UI.message("Do something")
+    } else {
+      sketch.UI.message("Not symbols")
+    }
+  
+ } else {
+    // When user didn't select anything
+    sketch.UI.message("Please select only two layers")
+  }
 
-  const imageURL = context.plugin.urlForResourceNamed('icon.png')
+  // const imageURL = context.plugin.urlForResourceNamed('icon.png')
 
 
-  const group = new sketch.Group({
-    parent: page,
-    name: 'arrows',
-    frame: {
-      x: 0,
-      y: 0,
-      width: 200,
-      height: 200,
-    },
-    layers: [
-      // you can also define nested layers directly
-      {
-        type: sketch.Types.Image,
-        frame: {
-          x: 50,
-          y: 50,
-          width: 100,
-          height: 100,
-        },
-        image: imageURL,
-      },
-    ],
-  })
+  // const group = new sketch.Group({
+  //   parent: page,
+  //   name: 'arrows',
+  //   frame: {
+  //     x: 0,
+  //     y: 0,
+  //     width: 200,
+  //     height: 200,
+  //   },
+  //   layers: [
+  //     // you can also define nested layers directly
+  //     {
+  //       type: sketch.Types.Image,
+  //       frame: {
+  //         x: 50,
+  //         y: 50,
+  //         width: 100,
+  //         height: 100,
+  //       },
+  //       image: imageURL,
+  //     },
+  //   ],
+  // })
 }
 
 export function updateArrows(context) {
--- userflows/userflows.sketchplugin/Contents/Sketch/script.js
+++ userflows/userflows.sketchplugin/Contents/Sketch/script.js
@@ -109,32 +109,47 @@ var _require = __webpack_require__(/*! util */ "util"),
     toArray = _require.toArray;
 
 /* harmony default export */ __webpack_exports__["default"] = (function () {
+  // Predefing
   var document = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.fromNative(context.document);
-  var page = document.selectedPage;
-  var selection = document.selectedLayers;
-  log(selection);
-  var imageURL = context.plugin.urlForResourceNamed('icon.png');
-  var group = new sketch__WEBPACK_IMPORTED_MODULE_0___default.a.Group({
-    parent: page,
-    name: 'arrows',
-    frame: {
-      x: 0,
-      y: 0,
-      width: 200,
-      height: 200
-    },
-    layers: [// you can also define nested layers directly
-    {
-      type: sketch__WEBPACK_IMPORTED_MODULE_0___default.a.Types.Image,
-      frame: {
-        x: 50,
-        y: 50,
-        width: 100,
-        height: 100
-      },
-      image: imageURL
-    }]
-  });
+  var page = document.selectedPage; // var selection = document.selectedLayers
+
+  var selection = context.selection;
+
+  if (selection.count() == 2) {
+    // When user selected two layers
+    if (selection[1].class() == "MSSymbolInstance") {
+      sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Do something");
+    } else {
+      sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Not symbols");
+    }
+  } else {
+    // When user didn't select anything
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Please select only two layers");
+  } // const imageURL = context.plugin.urlForResourceNamed('icon.png')
+  // const group = new sketch.Group({
+  //   parent: page,
+  //   name: 'arrows',
+  //   frame: {
+  //     x: 0,
+  //     y: 0,
+  //     width: 200,
+  //     height: 200,
+  //   },
+  //   layers: [
+  //     // you can also define nested layers directly
+  //     {
+  //       type: sketch.Types.Image,
+  //       frame: {
+  //         x: 50,
+  //         y: 50,
+  //         width: 100,
+  //         height: 100,
+  //       },
+  //       image: imageURL,
+  //     },
+  //   ],
+  // })
+
 });
 function updateArrows(context) {
   var document = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.fromNative(context.document);
